208ef931d622a0daf07201b4f64113ddb68cbebf
vijaychavrePetco <vijay.chavre@petco.com>
2020-12-25 21:32:16 +0530

    Refactored footer UI

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,12 +11,8 @@
     >
       <v-list-item>
         <v-list-item-content>
-          <v-list-item-title class="title">
-            Application
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            subtext
-          </v-list-item-subtitle>
+          <v-list-item-title class="title"> Application </v-list-item-title>
+          <v-list-item-subtitle> subtext </v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
 
@@ -38,39 +34,23 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
+    <v-app-bar :clipped-left="clipped" fixed app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-spacer></v-spacer>
-       <v-menu
-        bottom
-        min-width="200px"
-        rounded
-        offset-y
-      >
+
+      <!-- Avatar -->
+      <v-menu bottom min-width="200px" rounded offset-y>
         <template v-slot:activator="{ on }">
-          <v-btn
-            icon
-            x-large
-            v-on="on"
-          >
-            <v-avatar
-            color="grey lighten-1"
-              size="48"
-            >
-              <span class="white--text ">{{ user.initials }}</span>
+          <v-btn icon x-large v-on="on">
+            <v-avatar color="grey lighten-1" size="36">
+              <span class="white--text">{{ user.initials }}</span>
             </v-avatar>
           </v-btn>
         </template>
         <v-card>
           <v-list-item-content class="justify-center">
             <div class="mx-auto text-center">
-              <v-avatar
-                color="grey lighten-2"
-              >
+              <v-avatar color="grey lighten-2">
                 <span class="white--text">{{ user.initials }}</span>
               </v-avatar>
               <h3>{{ user.fullName }}</h3>
@@ -78,21 +58,9 @@
                 {{ user.email }}
               </p>
               <v-divider class="my-3"></v-divider>
-              <v-btn
-                depressed
-                rounded
-                text
-              >
-                Edit Account
-              </v-btn>
+              <v-btn depressed rounded text> Edit Account </v-btn>
               <v-divider class="my-3"></v-divider>
-              <v-btn
-                depressed
-                rounded
-                text
-              >
-                Disconnect
-              </v-btn>
+              <v-btn depressed rounded text> Disconnect </v-btn>
             </div>
           </v-list-item-content>
         </v-card>
@@ -107,39 +75,38 @@
     </v-main>
 
     <!-- Footer -->
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+    <v-footer :padless="true" :clipped-left="clipped">
+      <v-card flat tile width="100%" class="grey lighten-5 text-center"
+        >&copy; {{ new Date().getFullYear() }}</v-card
+      >
     </v-footer>
   </v-app>
 </template>
 
 <script>
 export default {
-  middleware:['isAuth'],
-  data () {
+  middleware: ["isAuth"],
+  data() {
     return {
       clipped: false,
       drawer: false,
-       user: {
-        initials: 'VC',
-        fullName: 'Vijay Chavre',
-        email: 'chavre.vijay@gmail.com',
+      user: {
+        initials: "VC",
+        fullName: "Vijay Chavre",
+        email: "chavre.vijay@gmail.com",
       },
       items: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        }
+          icon: "mdi-apps",
+          title: "Welcome",
+          to: "/",
+        },
       ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
-    }
-  }
-}
+      title: "Vuetify.js",
+    };
+  },
+};
 </script>
